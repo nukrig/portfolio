d744323419fe548fb528ff98ef6a0ca4f24851e8
nukrig <n_gegelashvili@cu.edu.ge>
2023-07-13 14:58:06 +0400

    added new responsive project

--- src/Components/MyProjects/MyProjects.js
+++ src/Components/MyProjects/MyProjects.js
@@ -3,6 +3,7 @@ import './MyProjects.css'
 import {FaCss3, FaHtml5, FaJs, FaReact } from 'react-icons/fa'
 import ProjectCard from "./ProjectCard";
 // Images
+import giksportfolio from '../../assets/giiks-portfolio.png'
 import liveCoding from '../../assets/unilab-livecoding.png'
 import ToDoApp from '../../assets/ToDo App.png'
 import TipCalc from '../../assets/TipCalc.png'
@@ -26,8 +27,18 @@ function MyProjects() {
         <FaReact size={30} style={{color:'white',marginRight:'1rem'}} />
         </div>
         <div className="project-container">
-
 {/* PROJECT 1 */}
+<ProjectCard 
+                img={giksportfolio} 
+                view='https://nukrig.github.io/giiks-portfolio/'
+                source='https://github.com/nukrig/giiks-portfolio'
+                heading='Fully responsive Project'
+                paragraph="This is my first real project that I made according to a visual made by a UI/UX designer.
+                 (You can see figma LINK in my github repository's README file)
+                 I think it was quite interesting and I also got some experience how to work as a team and so on. 
+                 In addition, I also worked on the responsive design and in the end the project is fully responsive and smooth. "
+            />
+{/* PROJECT 2 */}
 <ProjectCard 
                 img={liveCoding} 
                 view='https://nukrig.github.io/live-coding-unilab/'
@@ -41,7 +52,7 @@ function MyProjects() {
                 and this way i can set undo and redo button clicks ... "
             />
 
-{/* PROJECT 2 */}
+{/* PROJECT 3 */}
             <ProjectCard 
                 img={ToDoApp} 
                 view='https://nukrig.github.io/todo-app-unilab/'
@@ -55,7 +66,7 @@ function MyProjects() {
                 be checked whether he is logged out or not, and then the corresponding command 
                 should be executed."
             />
-{/* PROJECT 3 */}
+{/* PROJECT 4 */}
             <ProjectCard 
                 img={Cv}
                 view="https://nukrig.github.io/CV-Generator-1920x1080-/"
@@ -68,7 +79,7 @@ function MyProjects() {
                  1920x1080px. This project taught me a lot, but I couldn't move to the next 
                  stage of the Redberry internship because I didn't know React."
             />
-{/* PROJECT 4 */}
+{/* PROJECT 5 */}
             <ProjectCard 
                 img={TipCalc}
                 view="https://nukrig.github.io/tip-calculator-react/"
@@ -78,7 +89,7 @@ function MyProjects() {
                 lot of things in the direction of React,I used Props, State and other 
                 main concepts and in the end it turned out to be a good exercise ."
             />
-{/* PROJECT 5 */}
+{/* PROJECT 6 */}
             <ProjectCard 
                 img={Rating}
                 view="https://nukrig.github.io/interactive-rating-react/"
@@ -88,7 +99,7 @@ function MyProjects() {
                 as a small component, and use it in a relatively large project. This 
                 little task uses props to display the appropriate message on the next page."
             />
-{/* PROJECT 6 */}
+{/* PROJECT 7 */}
             <ProjectCard 
                 img={GithubUsers}
                 view="https://nukrig.github.io/react-usestate/"
@@ -97,7 +108,7 @@ function MyProjects() {
                 paragraph="In this example, I discussed how I could use fetch in React. 
                 I sent a request to the GitHub app and listed the appropriate users"
             />
-{/* PROJECT 7 */}
+{/* PROJECT 8 */}
             <ProjectCard 
                 img={Clock}
                 view="https://nukrig.github.io/react-lifecycle/"
@@ -106,7 +117,7 @@ function MyProjects() {
                 paragraph="In this example, I learned what the React lifecycle is, and I also 
                 built a clock application using UseEffect Hook."
             />
-{/* PROJECT 8 */}
+{/* PROJECT 9 */}
             <ProjectCard 
                 img={Products}
                 view="https://nukrig.github.io/function-components/"
@@ -115,7 +126,7 @@ function MyProjects() {
                 paragraph="This exercise is similar to a shopping cart in an online store,
                 you can add or remove specific products to the cart"
             />
-{/* PROJECT 9 */}
+{/* PROJECT 10 */}
             <ProjectCard 
                 img={Faq}
                 view="https://nukrig.github.io/FAQ-accordion/"
@@ -124,7 +135,7 @@ function MyProjects() {
                 paragraph="This exercise is from frontendmentor, I built an accordion
                 for Frequently asked questions..."
             />
-{/* PROJECT 10 */}
+{/* PROJECT 11 */}
             <ProjectCard 
                 img={Calculator}
                 view="https://nukrig.github.io/calculatorJs/"
@@ -132,7 +143,7 @@ function MyProjects() {
                 heading="Calculator (JS)"
                 paragraph="This is a simple calculator that I built on my own inspiration using JavaScript"
             />
-{/* PROJECT 11 */}
+{/* PROJECT 12 */}
             <ProjectCard 
                 img={CharCalc}
                 view="https://nukrig.github.io/numberOfCharacters/"
@@ -141,7 +152,7 @@ function MyProjects() {
                 paragraph="In this example, I created an application that allows you to specify a text as well as a 
                 letter and count the number of times the corresponding letter is used in that text."
             />
-{/* PROJECT 12 */}
+{/* PROJECT 13 */}
             <ProjectCard 
                 img={Responsive}
                 view="https://nukrig.github.io/PROJECT-RESPONSIVE/#"
